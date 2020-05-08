29ea764660fb25f02bba908a8c69ffccc67570e5
nhat51 <65001571+nhat51@users.noreply.github.com>
2020-05-08 11:38:47 +0700

    Create baitap.c

---
+++ baitap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int tuoi;
+	float luong;
+	
+	printf("nhap so tuoi: ");
+	scanf ("%d",&tuoi);
+	printf ("nhap so luong: ");
+	scanf ("%f",&luong);
+	
+	
+	return 0;
+}
